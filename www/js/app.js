bbd0ddb8627bdc9094a8be21d843727d4d59b62e
AppleChen66 <1138484114@qq.com>
2017-01-13 18:19:06 +0800

    Test commit

--- www/js/app.js
+++ www/js/app.js
@@ -8,6 +8,7 @@
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
+  console.log("HelloWorld")
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
